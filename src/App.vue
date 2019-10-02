b1eca3395385481ce07bd9e7c83ad70bc2394006
pen <760710450@qq.com>
2019-10-02 13:49:51 +0800

    header-commit-one

--- src/App.vue
+++ src/App.vue
@@ -24,5 +24,8 @@ export default {
 </script>
 
 <style>
-
+*{
+  margin: 0;
+  padding: 0;
+}
 </style>
--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,10 +1,16 @@
 <template>
+  <div class="header">
+    <a class="title" href="#">
+      <p class="big-title">豆芽仔.Pen</p>
+      <p class="small-title">邱品的个人博客</p>
+    </a>
     <nav>
       <ul>
         <router-link to="/" tag="li" exact>博客</router-link>
         <router-link to="/add" tag="li" exact>写博客</router-link>
       </ul>
     </nav>
+  </div>
 </template>
 
 <script>
@@ -14,8 +20,31 @@
 </script>
 
 <style scoped>
-nav{
-  background: crimson;
+.title{
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  color: #fff;
+  padding: 20px;
+  letter-spacing: 1px;
+  text-decoration: none;
+}
+.big-title{
+  margin-right: 10px;
+  font-size: 40px;
+  transition: letter-spacing 1s;
+}
+.title:hover .big-title{
+  letter-spacing: 10px;
+}
+.title:hover .small-title{
+  opacity: 0.5;
+}
+.small-title{
+  transition: opacity 1s;
+}
+.header{
+  background: #d56464;
   padding: 2px;
 }
 ul{
